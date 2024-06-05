bbf182d4c3a8ce20178dc023f59545e2b9f4d578
root <root@DESKTOP-48LG0GG.localdomain>
2024-06-05 09:49:13 +0900

    token embedding pad idx 수정

--- models/embedding/token_embeddings.py
+++ models/embedding/token_embeddings.py
@@ -1,4 +1,5 @@
 from torch import nn
+from data import *
 
 class TokenEmbedding(nn.Embedding):
     def __init__(self, vocab_size, d_model):
@@ -6,4 +7,4 @@ class TokenEmbedding(nn.Embedding):
         :param vocab_size: size of the dictionary of embeddings
         :param d_model: the size of each embedding vector
         """
-        super().__init__(num_embeddings=vocab_size, embedding_dim=d_model, padding_idx=1)
+        super().__init__(num_embeddings=vocab_size, embedding_dim=d_model, padding_idx=tokenizer.pad_token_id)
